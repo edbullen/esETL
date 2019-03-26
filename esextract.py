256506a104027c67a844cac4aa0b1627131eee13
edbullen <ed.bullen@gmail.com>
2019-03-26 16:01:51 +0000

    only print duplicate count if dup's found

--- esextract.py
+++ esextract.py
@@ -189,8 +189,8 @@ def create_dataframe(extract, cols_file=None, cols=None, drop_duplicates=True):
         #duplicates = dataframe.duplicated()
         duplicates = dataframe.astype(str).duplicated()
         duplicate_count = len(duplicates[duplicates].index)
-        log("    " + str(duplicate_count) + " duplicates found in dataframe")
-        if duplicate_count > 0 :
+        if duplicate_count > 0
+            log("   " + str(duplicate_count) + " duplicates found in dataframe")
             log(duplicates[duplicates].index)
         n_no_dup = len(dataframe)
 
